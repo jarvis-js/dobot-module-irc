c2536e8d1510a202aad9f2c01705e0a90e1aff85
Alex Whitman <alex@alexwhitman.com>
2011-10-31 17:05:32 +0000

    Implement DoBot.Module

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
+var irc = require('irc');
+
 module.exports = function(bot) {
 
-	var irc = require('irc');
-	
-	var client = new irc.Client('irc.freenode.net', 'dobot', {
-	    channels: [ '#dobot' ],
-		userName: 'dobot',
-		realName: 'dobot'
-	});
-	
-	client.addListener('message', function (from, channel, message) {
-		var regex = /^dobot,? /i;
-		if (regex.test(message)) {
-			message = message.replace(regex, '');
+	ircModule = new bot.Module();
+
+	ircModule.load = function() {
+
+		var self = this;
+
+		this.client = new irc.Client('irc.freenode.net', 'dobot', {
+			channels: [ '#dobot' ],
+			userName: 'dobot',
+			realName: 'dobot'
+		});
+
+		this.client.addListener('message', function (from, channel, message) {
+			var regex = /^dobot,? /i;
+			if (regex.test(message)) {
+				message = message.replace(regex, '');
+				var request = {
+					user: from,
+					command: message
+				};
+				bot.exec(request, function(response) {
+					self.client.say(channel, response);
+				});
+			}
+		});
+
+		this.client.addListener('pm', function(from, message) {
 			var request = {
 				user: from,
 				command: message
 			};
 			bot.exec(request, function(response) {
-				client.say(channel, response);
-			});
-		}
-	});
-	
-	client.addListener('pm', function(from, message) {
-		var request = {
-			user: from,
-			command: message
-		};
-		bot.exec(request, function(response) {
-			client.say(from, response);
-		})
-	});
+				self.client.say(from, response);
+			})
+		});
+
+	}
+
+	ircModule.unload = function() {
+		this.client.disconnect();
+	}
+
+	return ircModule;
 
 }
