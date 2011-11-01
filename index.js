19fedbc435af18837ba92f1325d25d9e9d356b03
Alex Whitman <alex@alexwhitman.com>
2011-11-01 12:26:16 +0000

    Use options in IRC module to specify connections

--- index.js
+++ index.js
@@ -4,45 +4,49 @@ module.exports = function(bot) {
 
 	ircModule = new bot.Module();
 
-	ircModule.load = function() {
-
+	ircModule.load = function(options) {
+		this.clients = [];
 		var self = this;
+		if (options.connections) {
+			for (var i = 0; i < options.connections.length; i++) {
+				var current = options.connections[i];
+				var client = new irc.Client(current.host, current.nick, current.options);
+				this.clients.push(client);
 
-		this.client = new irc.Client('irc.freenode.net', 'dobot', {
-			channels: [ '#dobot' ],
-			userName: 'dobot',
-			realName: 'dobot'
-		});
+				client.addListener('message', function (from, channel, message) {
+					var client = this;
+					var regex = /^dobot,? /i;
+					if (regex.test(message)) {
+						message = message.replace(regex, '');
+						var request = {
+							user: from,
+							command: message
+						};
+						bot.exec(request, function(response) {
+							client.say(channel, response);
+						});
+					}
+				});
 
-		this.client.addListener('message', function (from, channel, message) {
-			var regex = /^dobot,? /i;
-			if (regex.test(message)) {
-				message = message.replace(regex, '');
-				var request = {
-					user: from,
-					command: message
-				};
-				bot.exec(request, function(response) {
-					self.client.say(channel, response);
+				client.addListener('pm', function(from, message) {
+					var client = this;
+					var request = {
+						user: from,
+						command: message
+					};
+					bot.exec(request, function(response) {
+						client.say(from, response);
+					})
 				});
 			}
-		});
-
-		this.client.addListener('pm', function(from, message) {
-			var request = {
-				user: from,
-				command: message
-			};
-			bot.exec(request, function(response) {
-				self.client.say(from, response);
-			})
-		});
-
-	}
+		}
+	};
 
 	ircModule.unload = function() {
-		this.client.disconnect();
-	}
+		for (var i = 0; i < this.clients.length; i++) {
+			this.clients[i].disconnect();
+		}
+	};
 
 	return ircModule;
 
