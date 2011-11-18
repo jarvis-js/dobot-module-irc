1b38e23ef99e1152356574b9861d90be85b94bd5
Alex Whitman <alex@alexwhitman.com>
2011-11-18 15:28:19 +0000

    Improve irc module
    - Use git version of node-irc as version in npm is missing some features

--- index.js
+++ index.js
@@ -3,88 +3,81 @@ var irc = require('irc');
 module.exports = function(bot, module) {
 
 	module.load = function(options) {
-		module.clients = [];
-
+		module.adaptors = [];
 		if (options.connection) {
-			if (!Array.isArray(options.connection)) {
+			if(!Array.isArray(options.connection)) {
 				options.connection = [ options.connection ];
 			}
 			for (var i = 0; i < options.connection.length; i++) {
-				var current = options.connection[i];
-				current.options.stripColors = true;
-				var client = new irc.Client(current.host, current.nick, current.options);
-				module.clients.push(client);
-
-				client.addListener('message', function(from, to, message) {
-					var channel = module.getChannel(module.makeChannelIdentifier(client, to));
-					if (channel !== false) {
-						var messageData = {
-							message: message,
-							usernick: from
-						};
-						channel.emit('message', messageData);
-						var regex = new RegExp('^' + client.nick + ',? ', 'i');
-						if (regex.test(message)) {
-							messageData.message = messageData.message.replace(regex, '');
-							channel.emit('command', messageData);
-						}
-					}
-				});
-
-				client.addListener('pm', function(from, message) {
-					var channel = module.addChannel(module.makeChannelIdentifier(client, from), function(response) {
-						client.say(from, response.reply);
-					});
-					var messageData = {
-						message: message,
-						usernick: from
-					};
-					channel.emit('message', messageData);
-					channel.emit('command', messageData);
-				});
-
-				client.addListener('join', function(joinedChannel, nick) {
-					if (nick === client.nick) {
-						module.addChannel(module.makeChannelIdentifier(client, joinedChannel), function(response) {
-							var message = response.reply;
-							if (response.type === 'command') {
-								message = response.usernick + ': ' + message;
-							}
-							client.say(joinedChannel, message);
-						});
-					}
-				});
+				module.adaptors.push(new IRCAdaptor(bot, options.connection[i]));
 			}
 		}
 	};
 
 	module.unload = function() {
-		for (var i = 0; i < module.clients.length; i++) {
-			module.clients[i].disconnect();
+		for (var i = 0; i < module.adaptors.length; i++) {
+			module.adaptors[i].disconnect();
 		}
 	};
 
-	module.makeChannelIdentifier = function(client, channel) {
-		return 'irc:' + client.opt.server + ':' + channel;
-	}
+};
 
-	module.addChannel = function(channelID, say) {
-		if (bot.channels[channelID]) {
-			return bot.channels[channelID];
+function IRCAdaptor(bot, options) {
+	this.bot = bot;
+	this.options = options;
+	this.options.stripColors = true;
+	this.client = new irc.Client(this.options.host, this.options.nick, this.options.options);
+	var _this = this;
+	this.client.on('message#', function(from, to, text, message) { _this.channelMessage(from, to, text, message); });
+	this.client.on('pm', function(from, text, message) { _this.privateMessage(from, text, message); });
+};
+
+IRCAdaptor.prototype.channelMessage = function(from, to, text, message) {
+	var _this = this;
+	var channel = this.getChannel('irc:' + this.client.opt.server + ':' + to, function(response) {
+		var reply = response.reply;
+		if (response.type === 'command') {
+			reply = response.usernick + ': ' + reply;
 		}
-		var channel = new bot.Channel();
-		channel.module = module.name;
-		channel.identifier = channelID;
-		channel.say = say;
-		bot.registerChannel(channel);
-		return channel;
+		_this.client.say(to, reply);
+	});
+	var messageData = {
+		message: text,
+		usernick: from
+	};
+	channel.emit('message', messageData);
+	var regex = new RegExp('^' + this.client.nick + ',? ', 'i');
+	if (regex.test(messageData.message)) {
+		messageData.message = messageData.message.replace(regex, '');
+		channel.emit('command', messageData);
 	}
+};
 
-	module.getChannel = function(channelID) {
-		if (bot.channels[channelID]) {
-			return bot.channels[channelID];
-		}
-		return false;
+IRCAdaptor.prototype.privateMessage = function(from, text, message) {
+	var _this = this;
+	var channel = this.getChannel('irc:' + this.client.opt.server + ':' + from, function(response) {
+		_this.client.say(from, response.reply);
+	});
+	var messageData = {
+		message: text,
+		usernick: from
+	};
+	channel.emit('message', messageData);
+	channel.emit('command', messageData);
+};
+
+IRCAdaptor.prototype.getChannel = function(channelID, say) {
+	if (this.bot.channels[channelID]) {
+		return this.bot.channels[channelID];
 	}
+	var channel = new this.bot.Channel();
+	channel.module = module.name;
+	channel.identifier = channelID;
+	channel.say = say;
+	this.bot.registerChannel(channel);
+	return channel;
+};
 
+IRCAdaptor.prototype.disconnect = function() {
+	this.client.disconnect();
 };
